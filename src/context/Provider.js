3fc3f9fc4046bbdccc20528ae48b741428e63b6e
Lorene Pecci <lorenepecci@gmail.com>
2022-03-28 16:49:14 -0300

    req0

---
+++ src/context/Provider.js
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import Context from './Context';
+
+const Provider = ({ children }) => {
+  const contextData = {};
+  return (
+    <Context.Provider value={ { contextData } }>
+      { children }
+    </Context.Provider>
+  );
+};
+Provider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Provider;
---
+++ src/pages/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Login() {
+  /*  const { state } = useContext(Context); */
+
+  return (
+    <div>
+      Login
+    </div>
+  );
+}
