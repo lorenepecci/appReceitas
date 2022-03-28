28757495d09e2d295641e711c7f2dfe44b037f50
Patrick <patrickbastosc@gmail.com>
2022-03-28 18:49:26 -0300

    finalizado: Requisito concluído

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <div>
+      <div data-testid="profile-top-btn" />
+      <h1 data-testid="page-title">Foods</h1>
+      <div data-testid="search-top-btn" />
+    </div>
+  );
+}
