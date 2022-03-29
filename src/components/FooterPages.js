2c0eb4e19d7827ee3e6527283e22fe75a37a870c
kesley Muniz <contato.kgmstudios@hotmail.com>
2022-03-29 13:34:14 -0300

    Status(Finalizado)

--- src/components/FooterPages.js
+++ src/components/FooterPages.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Drinks from '../images/drinkIcon.svg';
 import Explore from '../images/exploreIcon.svg';
 import Foods from '../images/mealIcon.svg';
@@ -7,19 +8,25 @@ import './Footer.css';
 const FooterPages = () => (
   <div>
     <footer id="footer-page" data-testid="footer">
-      <button type="button">
-        <img
-          data-testid="drinks-bottom-btn"
-          src={ Drinks }
-          alt="Icone de bebidas"
-        />
-      </button>
-      <button type="button">
-        <img data-testid="explore-bottom-btn" src={ Explore } alt="Icone de explorar" />
-      </button>
-      <button data-testid="food-bottom-btn" type="button" src={ Foods }>
-        <img data-testid="food-bottom-btn" src={ Foods } alt="Icone de comidas" />
-      </button>
+      <Link to="/drinks">
+        <button type="button">
+          <img
+            data-testid="drinks-bottom-btn"
+            src={ Drinks }
+            alt="Icone de bebidas"
+          />
+        </button>
+      </Link>
+      <Link to="/explore">
+        <button type="button">
+          <img data-testid="explore-bottom-btn" src={ Explore } alt="Icone de explorar" />
+        </button>
+      </Link>
+      <Link to="/foods">
+        <button data-testid="food-bottom-btn" type="button" src={ Foods }>
+          <img data-testid="food-bottom-btn" src={ Foods } alt="Icone de comidas" />
+        </button>
+      </Link>
     </footer>
   </div>
 );
