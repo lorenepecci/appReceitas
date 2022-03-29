120549c04bb6f4bc5de3d73cc4ae38e5ea9fcf06
Iara Pedrosa <iaraferreirahg@gmail.com>
2022-03-28 21:53:36 -0300

    requisitos 2, 3 e 4 (finalizados)

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,10 +2,32 @@ import React from 'react';
 
 export default function Login() {
   /*  const { state } = useContext(Context); */
-
   return (
-    <div>
-      Login
-    </div>
+    <form>
+      <label htmlFor="email">
+        Email:
+        <input
+          type="email"
+          data-testid="email-input"
+          name="email"
+          id="email"
+        />
+      </label>
+      <label htmlFor="password">
+        Senha:
+        <input
+          type="password"
+          data-testid="password-input"
+          name="password"
+          id="password"
+        />
+      </label>
+      <button
+        data-testid="login-submit-btn"
+        type="button"
+      >
+        Enter
+      </button>
+    </form>
   );
 }
