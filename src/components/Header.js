d7eedd06b9c6bda0d240c0f4de564a6ea2789058
Patrick <patrickbastosc@gmail.com>
2022-03-29 15:43:21 -0300

    refatorado: Redirecionamento para página Profile.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
 export default function Header({ title }) {
+  const history = useHistory();
+
   return (
     <div>
-      <Link to="/profile">
-        <button
-          type="button"
-          // onClick={ () => history.push('/profile') }
-          // onClick={ () => {} }
-        >
-          <img
-            data-testid="profile-top-btn"
-            src={ profileIcon }
-            alt="Ícone do Perfil"
-          />
-        </button>
-      </Link>
+      {/* <Link to="/profile"> */}
+      <button
+        type="button"
+        onClick={ () => history.push('/profile') }
+      >
+        <img
+          data-testid="profile-top-btn"
+          src={ profileIcon }
+          alt="Ícone do Perfil"
+        />
+      </button>
       <h1 data-testid="page-title">{ title }</h1>
       <img
         data-testid="search-top-btn"
