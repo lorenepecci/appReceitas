08f6827d6016c6b0153ebaea0c48425890b9d84d
Patrick <patrickbastosc@gmail.com>
2022-03-29 19:16:39 -0300

    finalizado: Requisito 12 concluído

--- src/pages/Foods.js
+++ src/pages/Foods.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
-import SearchBar from '../components/SearchBar';
 import FooterPages from '../components/FooterPages';
 
 export default function Foods() {
   return (
     <div>
-      <Header title="Foods" showSearchIcon />
+      <Header title="Foods" foodOrDrink="food" showSearchIcon />
       Foods
-      <SearchBar foodOrDrink="food" />
       <FooterPages />
     </div>
   );
