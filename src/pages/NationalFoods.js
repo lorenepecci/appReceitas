473a8d53fb4beb0053dca4c5f5e7ceba1e4f9dd1
kesley Muniz <contato.kgmstudios@hotmail.com>
2022-04-02 02:31:48 -0300

    Removendo warnings dos arquivos

--- src/pages/NationalFoods.js
+++ src/pages/NationalFoods.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
 import FooterPages from '../components/FooterPages';
 import Header from '../components/Header';
